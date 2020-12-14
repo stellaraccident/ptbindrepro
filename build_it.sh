f677caec072d37760b84a03cbac52415f2d3eda7
Stella Laurenzo <stellaraccident@gmail.com>
2020-12-13 16:59:19 -0800

    Add gcc and clang flags.

--- build_it.sh
+++ build_it.sh
@@ -13,13 +13,17 @@ LINK="$(python -c 'import sysconfig;print(sysconfig.get_config_var("LDCXXSHARED"
 # If those mismatch and it crosses a PYBIND11_INTERNALS_VERSION version bump,
 # modules won't be able to share types.
 
-FIX_FLAGS="-fabi-version=11 -DPYBIND11_COMPILER_TYPE=\"_gcc\""
+GCC_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -fabi-version=11"
+CLANG_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -U__GXX_ABI_VERSION -D__GXX_ABI_VERSION=1011 -DPYBIND11_COMPILER_TYPE=\"_gcc\""
 
 # Pybind forks its world based on what it computes for its PYBIND11_INTERNALS_ID.
 # Components of this that are likely to change on a Linux system are
 # PYBIND11_COMPILER_TYPE (_clang vs _gcc), PYBIND11_STDLIB (_libcpp vs _libstdcpp)
 # and PYBIND11_BUILD_ABI (_cxxabi1011 vs _cxxabi1013).
-g++ -c -o module.o -D_GLIBCXX_USE_CXX11_ABI=0 -I$TORCH_INCLUDE_DIR -I$PY_INCLUDE_DIR \
-  -fPIC -fvisibility=hidden -frtti -fexceptions $FIX_FLAGS module.cpp
+clang++ -c -o module.o -I$TORCH_INCLUDE_DIR -I$PY_INCLUDE_DIR \
+  -fPIC -fvisibility=hidden -frtti -fexceptions $CLANG_CXXFLAGS module.cpp
+# g++ -c -o module.o -I$TORCH_INCLUDE_DIR -I$PY_INCLUDE_DIR \
+#   -fPIC -fvisibility=hidden -frtti -fexceptions $GCC_CXXFLAGS module.cpp
+
 $LINK -o ptbindrepro.so -Wl,-rpath,$TORCH_LIB_DIR -L$TORCH_LIB_DIR -ltorch_python module.o \
   -ltorch_python -ltorch_cpu
